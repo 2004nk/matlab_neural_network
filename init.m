ece1deda325b5286ae0d411db3df26a9998627d0
2004nk <166636124+2004nk@users.noreply.github.com>
2024-04-26 19:42:16 +0530

    Update init.m

--- init.m
+++ init.m
@@ -1,17 +1,30 @@
 load("mnist.mat");
 X=training.images;
-Y=training.labels;
-for i=1:60000
+tot=60000;
+%batch_size=1000;
+epochs=1000;
+test_tot=10000;
+Y=test.images;
+img=zeros(784,tot);
+img_test=zeros(784,test_tot);
+for i=1:tot
     for j=1:27
         for k=1:28
             img((28*j+k),i)=X(j,k,i);
         end
     end
 end
-id=img(:,1);
+img1=cast(img,'single');
+for i=1:test_tot
+    for j=1:27
+        for k=1:28
+            img_test((28*j+k),i)=Y(j,k,i);
+        end
+    end
+end
 label=(training.labels)';
-responses=zeros(10,60000);
-for i=1:60000
+responses=zeros(10,tot);
+for i=1:tot
     x=label(1,i);
     if x>0
         responses(x,i)=1;
@@ -19,8 +32,23 @@ for i=1:60000
         responses(10,i)=1;
     end
 end
-layer1_weight=rand(32,784);
-layer2_weight=rand(10,32);
+res=cast(responses,'single');
+label_test=(test.labels)';
+res_test=zeros(10,test_tot);
+for i=1:test_tot
+    x=label_test(1,i);
+    if x>0
+        res_test(x,i)=1;
+    else
+        res_test(10,i)=1;
+    end
+end
+
+%image(X(:,:,1));
+layer1_weight=rand(100,784);
+layer2_weight=rand(10,100);
+layer1_bias=rand(100,1);
+layer2_bias=rand(10,1);
 clear X
 clear x
 clear Y
