8a28f6a27a1814d96ce1a03978c6cee2d91e8c6e
2004nk <166636124+2004nk@users.noreply.github.com>
2024-04-26 19:41:38 +0530

    Update backprop.m

--- backprop.m
+++ backprop.m
@@ -1,25 +1,22 @@
-function [layer1_weight,layer2_weight] = backprop(i,layer1_weight,layer2_weight,op,responses,actv1,img)
-    cost=0;
-    learn_rate=0.15;
-    for j= 1:10
-        cost=cost+(responses(j,i)-op(j,i)^2);
-    end
-    cost=cost/2;
-    del_weight2=zeros(10,32);
-    del_weight1=zeros(32,784);
-    for l=1:32
-        x=0;
-        for k=1:10
-            x=x+((op(k,i)-responses(k,i))*(op(k,i)*(1-op(k,i))));
-            del_weight2(k,l)=learn_rate*(x*(actv1(l,i)));
-        end
-        del_weight1(l,:)=x;
-    end
-    layer2_weight=layer2_weight-del_weight2;
-    for m=1:32
-        for n=1:784
-            del_weight1(m,n)=learn_rate*(del_weight1(m,n))*(actv1(m,n)*(1-actv1(m,n)))*(img(n,i));
-        end
-    end
-    layer1_weight=layer1_weight-del_weight1;
+function [layer1_weight, layer2_weight, layer1_bias, layer2_bias] = backprop(i, layer1_weight, layer2_weight, op, responses, actv1, img, layer1_bias, layer2_bias)
+    learn_rate = 1;
+    d_layer2weight = zeros(10, 100);
+    d_layer1weight = zeros(100, 784);
+    d_layer2bias = zeros(10, 1);
+    d_layer1bias = zeros(100, 1);
+
+    d=(op(:,i)-responses(:,i)).*(sigmoidprime(op(:,i)));
+
+    d_layer2weight=d*actv1(:,i)';
+    d_layer2bias=d;
+
+    d1=layer2_weight'*d.*sigmoidprime(actv1(:,i));
+    d_layer1weight=d1*sigmoid(img(:,i))';
+    d_layer1bias=d1;
+
+    layer1_weight=layer1_weight-(learn_rate/i)*d_layer1weight;
+    layer1_bias=layer1_bias-(learn_rate/i)*d_layer1bias;
+    layer2_weight=layer2_weight-(learn_rate/i)*d_layer2weight;
+    layer2_bias=layer2_bias-(learn_rate/i)*d_layer2bias;
+
 end
