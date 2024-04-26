b0c90dd2db8c56a6952b132b134d3a96cb2febe1
2004nk <166636124+2004nk@users.noreply.github.com>
2024-04-26 19:47:24 +0530

    Update nn_fp.m

--- nn_fp.m
+++ nn_fp.m
@@ -1,5 +1,5 @@
-function [oplayer,actv1] = nn_fp(actv,layer1_weight,layer2_weight)
-    actv1=nextlayer(actv,layer1_weight);
-    actv1=actv1';
-    oplayer=nextlayer(actv1,layer2_weight);
+function [oplayer,actva] = nn_fp(actv,layer1_weight,layer2_weight,layer1_bias,layer2_bias)
+    actva=nextlayer(actv,layer1_weight,layer1_bias);
+    %actv1=actv1';
+    oplayer=nextlayer(actva,layer2_weight,layer2_bias);
 end
